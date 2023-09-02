085d0cc397c5e1903ec3dd9774accdfaf89b6736
Benjamin Atlas <benjamin.s.atlas@gmail.com>
2023-09-02 11:49:39 -0600

    Added some typing animations (incomplete)

--- src/SimulatedTypingText.tsx
+++ src/SimulatedTypingText.tsx
@@ -1,3 +1,4 @@
+import { faCropSimple } from "@fortawesome/free-solid-svg-icons";
 import React, { useState, useEffect } from "react";
 
 const SimulatedTypingText = (props: { prefix: string; words: string[] }) => {
@@ -7,23 +8,54 @@ const SimulatedTypingText = (props: { prefix: string; words: string[] }) => {
 
   const [prefix, _setPrefix] = useState(props.prefix ? props.prefix : "I am a");
 
+  const [typedText, setTypedText] = useState("");
+  const [backspacing, setBackspacing] = useState(false);
+
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      const randomIndex = Math.floor(Math.random() * props.words.length);
-      const newText = props.words[randomIndex];
+    const intervalId: number = setInterval(() => {
+      const randomIndex: number = Math.floor(
+        Math.random() * props.words.length
+      );
+      const newText: string = props.words[randomIndex];
       setDisplayedPhrase(newText);
-    }, 3000);
+    }, 8000); // Compute the switch based on the longest word + characters per second + some delay
 
     // Clear the interval when the component unmounts to prevent memory leaks
     return () => clearInterval(intervalId);
   }, []); // Empty dependency array to run this effect only once on component mount
 
+  useEffect(() => {
+    const intervalId: number = setInterval(() => {
+      if (typedText === displayedPhrase) {
+        setBackspacing(true);
+      }
+
+      if (typedText === "") {
+        // Probably want to ask, "would the eventually-backspaced substr
+        // match a substr of the same length for the next queued up word?""
+        // to simulate real typing.
+        setBackspacing(false);
+      }
+
+      setTypedText(
+        displayedPhrase.substring(
+          0,
+          backspacing ? typedText.length - 1 : typedText.length + 1
+        )
+      );
+    }, 150);
+
+    return () => clearInterval(intervalId);
+  }, [typedText, backspacing]);
+
   return (
     <div>
       <div className="inline">
-        <div className="inline-block mr-2">{prefix}</div>
+        <div className="inline-block border-b-4 border-transparent mr-2">
+          {prefix}
+        </div>
         <div className="inline-block border-b-4 border-blue-300">
-          {displayedPhrase}
+          {typedText}
         </div>
       </div>
       <div className="border-r-4 border-white animate-fadeIn h-10 w-1 ml-1 inline"></div>
