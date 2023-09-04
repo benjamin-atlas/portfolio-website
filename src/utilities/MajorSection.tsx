e72803693787607ad50a44e67e602cd546300770
Benjamin Atlas <benjamin.s.atlas@gmail.com>
2023-09-04 15:53:59 -0600

    Added section utility component file

---
+++ src/utilities/MajorSection.tsx
@@ -0,0 +1,27 @@
+import SectionHeader from "./SectionHeader";
+
+const MajorSection = ({
+  sectionName,
+  bgColor,
+  header,
+  children,
+}: {
+  sectionName: string;
+  bgColor?: string;
+  header: string;
+  children: any;
+}) => {
+  return (
+    <section
+      id={`${sectionName}-section`}
+      className={`relative ${
+        bgColor ? bgColor : "bg-white"
+      } text-black font-raleway w-full text-xl pl-32 pr-32`}
+    >
+      <SectionHeader>{header}</SectionHeader>
+      {children}
+    </section>
+  );
+};
+
+export default MajorSection;
