b64859379288c8938079b5dcd81fd97fe1729892
Benjamin Atlas <benjamin.s.atlas@gmail.com>
2023-09-05 22:54:15 -0600

    Workaround for top styling on resume

--- src/utilities/ResumeItem.tsx
+++ src/utilities/ResumeItem.tsx
@@ -6,7 +6,7 @@ const ResumeItem = (props: { resumeProps: ResumeProps }) => {
     <div className="font-poppins font-thin border-l-4 border-blue-400 pl-4 relative">
       <ScrollTrigger
         initialClassName="rounded-full w-[20px] h-[20px] border-4 border-blue-400 absolute top-[100%] transition-[top] duration-[1000ms] ease-in-out left-[-2px] translate-x-[-50%] bg-white"
-        triggerClassName="top-[0%]"
+        triggerStyles={{ top: "0%" }}
       ></ScrollTrigger>
       <div className="mb-3 font-semibold">{props.resumeProps.header}</div>
       <div className="italic mb-3 text-base">{props.resumeProps.subheader}</div>
